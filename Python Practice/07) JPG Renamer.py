89dac1ba00c710d55e47e37e1d8a9e949612bda4
Kratu <kratugautam99@gmail.com>
2024-05-16 12:16:59 +0530

    commit deleted

--- Python Practice/07) JPG Renamer.py
+++
@@ -1,9 +0,0 @@
-import os
-os.chdir("test AVIFs")
-print(os.getcwd())
-i = 1
-for filename in os.listdir():
-    ext = os.path.splitext(filename)
-    os.rename(filename, str(i) + ext[1])
-    i += 1
-print(os.listdir())
