aca5b695ba6a82ee7b8f1103ae2a8e1d929dd733
Kratu Gautam <kratugautam99@gmail.com>
2026-08-18 04:24:28 +0530

    Update Query 10.sql

--- SQL Works/Solutions/Query 10.sql
+++ SQL Works/Solutions/Query 10.sql
@@ -1,7 +1,17 @@
-with temp1 as (select dim_product.product_code, division, product, 
-sum(sold_quantity) as total_sold_quantity from dim_product join fact_sales_monthly
-on dim_product.product_code = fact_sales_monthly.product_code where fiscal_year = 2021 
-group by dim_product.product_code, product, division 
-), temp2 as (select *,row_number() over (partition by division 
-order by total_sold_quantity desc) as rank_order from temp1)
-select * from temp2 where rank_order<=3;
+WITH ranked_products AS (
+    SELECT 
+        p.product_code, 
+        p.division, 
+        p.product, 
+        SUM(s.sold_quantity) AS total_sold_quantity,
+        ROW_NUMBER() OVER (
+            PARTITION BY p.division 
+            ORDER BY SUM(s.sold_quantity) DESC
+        ) AS rank_order
+    FROM dim_product p
+    JOIN fact_sales_monthly s ON p.product_code = s.product_code
+    WHERE s.fiscal_year = 2021
+    GROUP BY p.product_code, p.division, p.product
+)
+SELECT * FROM ranked_products 
+WHERE rank_order <= 3;
